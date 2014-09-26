262ff6464c42670218d94b8c59b16e1ebc66f27a
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-09-26 11:14:16 -0400

    Adds Unity configuration package.

---
+++ src/FusionAlliance.Mediator.Unity.Tests/UnityMediatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FusionAlliance.Mediator.Common;
+using FusionAlliance.Mediator.Common.Tests;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+
+namespace FusionAlliance.Mediator.Unity.Tests
+{
+    [TestFixture]
+    public class UnityMediatorTests : IDisposable
+    {
+        private UnityContainer _container;
+        private UnityMediator _mediator;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IRequestHandler<DoubleInteger, DoubleIntegerReply>, DoubleIntegerHandler>();
+            _mediator = new UnityMediator(_container);
+        }
+
+        [Test]
+        public void It_can_resolve_a_request()
+        {
+            var doubleInteger = new DoubleInteger(6);
+            var reply = _mediator.Request(doubleInteger);
+            Assert.AreEqual(12, reply.DoubledValue);
+        }
+
+        [Test]
+        public void It_throws_an_exception_when_the_container_is_null()
+        {
+            IUnityContainer container = null;
+            Assert.Throws<ArgumentNullException>(() => new UnityMediator(container));
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                _container.Dispose();
+                _mediator.Dispose();
+            }
+        }
+    }
+}
---
+++ src/FusionAlliance.Mediator.Unity/UnityMediator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FusionAlliance.Mediator.Common;
+using Microsoft.Practices.Unity;
+
+namespace FusionAlliance.Mediator.Unity
+{
+    public class UnityMediator : AbstractMediator
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityMediator(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            _container = container;
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+        }
+
+        protected override object GetInstanceOfHandler(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
