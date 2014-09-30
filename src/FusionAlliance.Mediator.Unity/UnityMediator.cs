bc80026dd4e6c1394c459f55e74042e4dad7d51f
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-09-30 11:03:43 -0400

    Comments for UnityMediator.

--- src/FusionAlliance.Mediator.Unity/UnityMediator.cs
+++ src/FusionAlliance.Mediator.Unity/UnityMediator.cs
@@ -4,10 +4,17 @@ using Microsoft.Practices.Unity;
 
 namespace FusionAlliance.Mediator.Unity
 {
+    /// <summary>
+    /// Unity-specific implementation of the mediator pattern.
+    /// </summary>
     public class UnityMediator : AbstractMediator
     {
         private readonly IUnityContainer _container;
 
+        /// <summary>
+        /// Create a new instance of a <see cref="UnityContainer"/>.
+        /// </summary>
+        /// <param name="container">Unity container.</param>
         public UnityMediator(IUnityContainer container)
         {
             if (container == null)
